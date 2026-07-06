475417fb9719f803fe8c4d9df50895613c695463
Bad Coder <xpertespire@gmail.com>
2026-07-06 09:53:02 +0545

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
+interface Vehicle {
+    void start();
+    void stop();
+}
+
+class Car implements Vehicle {
+
+    public void start() {
+        System.out.println("Car has Started.");
+    }
+
+    public void stop() {
+        System.out.println("Car has Stopped.");
+    }
+}
+
+class Bike implements Vehicle {
+
+    public void start() {
+        System.out.println("Bike has Started.");
+    }
+
+    public void stop() {
+        System.out.println("Bike has Stopped.");
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Vehicle car = new Car();
+        car.start();
+        car.stop();
+
+        Vehicle bike = new Bike();
+        bike.start();
+        bike.stop();
     }
 }
